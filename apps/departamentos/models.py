4f222bed38b3dcef40113df976345b779a3e58e0
monteirolc <lucasm.castro47@gmail.com>
2022-03-16 12:33:30 -0300

    FEAT.: apps departamentos

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class Funcionario(models.Model):
-    name = models.CharField(max_length=100, verbose_name='nome')
+class Departamento(models.Model):
+    name = models.CharField(max_length=70, verbose_name='nome')
 
     def __str__(self):
         return self.name
--- company/settings.py
+++ company/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.empresas',
     'apps.funcionarios',
+    'apps.departamentos',
+
 ]
 
 MIDDLEWARE = [
