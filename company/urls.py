50f43e937686cb05f7e928e5395904e29430a131
monteirolc <lucasm.castro47@gmail.com>
2022-03-17 10:31:19 -0300

    FEAT.: apps core and apps/core/templates

--- company/urls.py
+++ company/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('funcionarios/', include('apps.funcionarios.urls')),
+    path('', include('apps.core.urls')),
 ]
