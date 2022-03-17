872ff37d1bdef0a543e7ff802fbeb23924557bc9
monteirolc <lucasm.castro47@gmail.com>
2022-03-17 10:22:21 -0300

    FEAT.: apps core and templates/base

--- company/settings.py
+++ company/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'apps.departamentos',
     'apps.documentos',
     'apps.registro_hora_extra',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'company.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
